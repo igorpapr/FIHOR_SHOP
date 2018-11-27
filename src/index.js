ca7654883cc8d6aefa42492f6368e86e8e145efa
Ihor Paprotskyi <igorpapr@gmail.com>
2018-11-28 01:18:23 +0200

    Added categories loaded from API

--- src/index.js
+++ src/index.js
@@ -2,3 +2,20 @@ import './scss/main.scss';
 
 console.log('Hello!');
 console.log(`The time is ${new Date()}`);
+
+$('document').ready(function () {
+    loadCategories();
+})
+
+function loadCategories() {
+    //loading categories to the list of categories
+    var categoriesAPI = "https://nit.tron.net.ua/api/category/list";
+    $.getJSON(categoriesAPI,function (data) {
+        //console.log(data);
+        var out = '';
+        for (var key in data){
+            out+='<li class="category-item" title="'+data[key]['description']+'">'+data[key]['name']+'</li>';
+        }
+        $('#categories-list ul').html(out);
+    });
+}
