706e7e4ff62e3fa7c1ec2787c31edd8db09a77c6
Ihor Paprotskyi <igorpapr@gmail.com>
2018-11-28 19:33:40 +0200

    Added cart

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './scss/main.scss';
 
-console.log('Hello!');
-console.log(`The time is ${new Date()}`);
+//stash
+var cart = {};
 
 $('document').ready(function () {
     loadCategories();
@@ -25,6 +25,7 @@ function loadGoods() {
     $.getJSON("https://nit.tron.net.ua/api/product/list",function (data) {
         var out = '';
         for (var key in data){
+            var goodsId = data[key]['id'];
             out+='<div class="goods-list">';
             var name = data[key]['name'];
             out+='<div class="goods-img-container">';
@@ -33,13 +34,25 @@ function loadGoods() {
             out+='<span class="goods-name">'+ name +'</span>';
             var price = data[key]['price'];
             var sprice = data[key]['special_price'];
-            //out+='<div class="price-container">';
             if (sprice!=null){
                 out+='<span class="special-price">'+ sprice + '</span>';
             }
             out+='<span class="price">'+price+'</span>';
+            out+='<button class="add-to-cart" data-id="'+ goodsId +'">Buy</button>';
             out+='</div>';
         }
         $('#goods-container').append(out);
+        $('button.add-to-cart').on('click', addToCart);
     });
+}
+
+function addToCart() {
+    var articul = $(this).attr('data-id');
+    if (cart[articul]!=undefined){
+        cart[articul]++;
+    }
+    else{
+        cart[articul] = 1;
+    }
+    console.log(cart);
 }
