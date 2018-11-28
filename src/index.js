9f317129edc39ef1b19e4354b6c23ab5d111f49d
Ihor Paprotskyi <igorpapr@gmail.com>
2018-11-28 04:12:33 +0200

    Added api goods loading

--- src/index.js
+++ src/index.js
@@ -5,17 +5,38 @@ console.log(`The time is ${new Date()}`);
 
 $('document').ready(function () {
     loadCategories();
+    loadGoods();
 })
 
 function loadCategories() {
     //loading categories to the list of categories
-    var categoriesAPI = "https://nit.tron.net.ua/api/category/list";
-    $.getJSON(categoriesAPI,function (data) {
-        //console.log(data);
+    $.getJSON("https://nit.tron.net.ua/api/category/list",function (data) {
         var out = '';
         for (var key in data){
             out+='<li class="category-item" title="'+data[key]['description']+'">'+data[key]['name']+'</li>';
         }
         $('#categories-list ul').html(out);
+        var curr = $('.category-item:contains("All")').addClass('current');
+        var desr = curr.attr('title');
+        $('#goods-content-header').html(desr);
+    });
+}
+function loadGoods() {
+    $.getJSON("https://nit.tron.net.ua/api/product/list",function (data) {
+        var out = '';
+        for (var key in data){
+            out+='<div class="goods-list">';
+            var name = data[key]['name'];
+            out+='<img class="goods-img" src="'+data[key]['image_url']+'" alt="'+ name +'">';
+            out+='<span class="goods-name">'+ name +'</span>';
+            var price = data[key]['price'];
+            var sprice = data[key]['special_price'];
+            if (sprice!=null){
+                out+='<span style="text-decoration:line-through">'+sprice+'</span>';
+            }
+            out+='<span class="price">'+price+'</span>';
+            out+='</div>';
+        }
+        $('#goods-content').append(out);
     });
 }
