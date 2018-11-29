dd6d3a136adbb351110892a3a0173c5773c40b65
Ihor Paprotskyi <igorpapr@gmail.com>
2018-11-29 23:08:18 +0200

    Added goods preview

--- src/index.js
+++ src/index.js
@@ -9,12 +9,21 @@ $('document').ready(function () {
     loadCategories();
     loadGoodsFromCategory(1);
     checkCart();
+    console.log(cart);
     $('.openbtn').on('click', function(){
-            openCart();
+        openCart();
     });
     $('.closebtn').on('click', function(){
         closeCart();
     });
+
+    // var modal = document.getElementById('preview');
+    // // When the user clicks anywhere outside of the modal, close it
+    // window.onclick = function(event) {
+    //     if (event.target == modal) {
+    //         modal.style.display = "none";
+    //     }
+    // }
     //showMiniCart();
 });
 
@@ -53,7 +62,9 @@ function loadGoodsFromCategory(id) {
     else         source ='https://nit.tron.net.ua/api/product/list/category/'+id;
 
     $.getJSON(source,function (data) {
-        goods = data;
+
+        goods = data;/////////////////////////////////all goods
+
         var out = '';
         for (var key in data){
             var goodsId = data[key]['id'];
@@ -62,7 +73,7 @@ function loadGoodsFromCategory(id) {
             out+='<div class="goods-img-container">';
             out+='<img class="goods-img" src="'+data[key]['image_url']+'" alt="'+ name +'">';
             out+='</div>';
-            out+='<span class="goods-name open-descr-frame" name-art="'+goodsId+'">'+ name +'</span>';
+            out+='<span class="goods-name show-good-info" name-art="'+goodsId+'">'+ name +'</span>';
             var price = data[key]['price'];
             var sprice = data[key]['special_price'];
             if (sprice!=null){
@@ -103,8 +114,109 @@ function loadGoodsFromCategory(id) {
                 showMiniCart();
             }
         });
+        $('.show-good-info').on('click', function(){
+            var id = $(this).attr('name-art');
+            showPreview(id);
+        });
     });
 }
+
+function showPreview(id) {
+    var link = 'https://nit.tron.net.ua/api/product/' + id;
+    $.getJSON(link, function (data) {
+        var out = '';
+
+        out += '<div id="id01" class="modal">';
+        out += '<div class="modal-content animate">';
+
+        out += '<div class="imgcontainer">';
+        out += '<span class="close" title="Close Preview">&times;</span>';
+        out += '<img class="avatar" src="' + data['image_url'] + '" alt="Goods image">';
+        out += '</div>';
+
+        out += '<div class="container">';
+        out += '<p /*         class       */>' + data['name'] + '</p>';/////////////////////////////////////////////////
+        out += '<span>Description</span>'
+        out += '<p /* class */>' + data['description'] + '</p>';
+        out += '</div>';
+
+        out += '<div class="container">';
+        out += '<p style="text-transform: uppercase; margin: 5px 10px;">Price</p>';
+
+        if (data['special_price'] === null) {
+            out += '<p class="price">' + data['price'] + ' грн. </p>';
+        }
+        else {
+            out += '<p class="old-price">' + data['price'] + ' грн. </p>';
+            out += '<p class="new-price">' + data['special_price'] + ' грн. </p>';
+        }
+        out += '</div>';
+
+        out += '<div class="container" style="background-color:#f1f1f1">';
+        out += '<button class="cancel-prev-btn" type="button">Cancel</button>';
+        out += '<button class="add-button" data-art="' + data['id'] + '"> Add to cart </button>';
+        out += '</div>';
+
+        out += '</div>';
+        out += '</div>';
+
+        $('#preview-frame').html(out);
+
+        document.getElementById('id01').style.display = 'block';
+
+       $('.cancel-prev-btn').on('click', function () {
+           document.getElementById("id01").style.display = "none";
+       });
+
+       $('.close').on('click', function () {
+           document.getElementById("id01").style.display = "none";
+       });
+
+       // Get the modal
+        var modal = document.getElementById('id01');
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+
+        $('.add-button').on('click', function(){
+            var id = $(this).attr('data-art');
+
+            if(cart[id]!=undefined){
+                cart[id].quantity++;
+            }
+            else{
+                cart[id]={};
+                cart[id].name = data['name'];
+                cart[id].quantity=1;
+                if(data['special_price']!=null){
+                    cart[id].price=data['special_price'];
+                }else{
+                    cart[id].price=data['price'];
+                }
+            }
+            localStorage.setItem('cart',JSON.stringify(cart));
+
+            if(!cartOpened){
+                $('#dropdown').addClass('drop');
+                $('.dd').addClass('dd-active');
+                cartOpened=true;
+                showMiniCart();
+            }
+            else{
+                showMiniCart();
+            }
+            modal.style.display = "none";
+            openCart();
+        });
+
+    });
+}
+
+
 function openCart() {
     showMiniCart();
     document.getElementById("mySidebar").style.width = "317px";
@@ -116,7 +228,13 @@ function closeCart() {
     document.getElementById('header').style.marginRight= "0";
     cartOpened = false;
 }
-
+function isEmpty(obj) {
+    for(var i in obj) {
+        if(obj.hasOwnProperty(i))
+            return false;
+    }
+    return JSON.stringify(obj) === JSON.stringify({});
+}
 function showMiniCart(){
     //show cart
     if (!(isEmpty(cart))){
@@ -126,7 +244,7 @@ function showMiniCart(){
             out += '<div class="cart-items">';
             out += '<img class="cross-img" src="img/cross.png" data-art = "'+x+'" >';
             out += '<div class="cart-name">';
-            out += '<span id="show-good-info" data-art = "'+x+'">' + goods[x].name + '</span></div>';
+            out += '<span id="show-good-info" data-art = "'+x+'">' + cart[x].name + '</span></div>';
             out += '<div class="cart-item-quantity">';
             out += '<img class="operations-img plus" src="img/plus.png"  data-art="'+x+'">';
             out += '<span>' + cart[x].quantity + '</span>';
@@ -136,7 +254,7 @@ function showMiniCart(){
             out += '</div>';
         }
         out += '<div class="cart-item-cost-buy">';
-        out += '<span class="cart-item-cost">Total: '+ total +'</span>';
+        out += '<span class="cart-item-cost">Total: '+ total +' UAH</span>';
         out += '<button class="cart-btn">Order</button>';
         out += '</div>';
         $('.cart-content').html(out);
@@ -146,27 +264,11 @@ function showMiniCart(){
         //     //showCheckoutDialog(currPrice);
         // });
         //
-        // $('p #show-good-info').on('click', function(){
+        // $('#show-good-info').on('click', function(){
         //     var id = $(this).attr('data-art');
         //     //cartDrop();
         //     //showGoodDialog(id);
         // });
-
-        $('.cross-img').on('click', function(){
-            var id = $(this).attr('data-art');
-            cart[id].quantity=0;
-            delete cart[id];
-            showMiniCart();
-            localStorage.setItem('cart',JSON.stringify(cart));
-        });
-        //
-        $('.plus').on('click', function(){
-            var id = $(this).attr('data-art');
-            cart[id].quantity++;
-            localStorage.setItem('cart',JSON.stringify(cart));
-            showMiniCart();
-        });
-
         $('.minus').on('click', function(){
             var id = $(this).attr('data-art');
             if(cart[id].quantity>1){
@@ -176,6 +278,20 @@ function showMiniCart(){
             localStorage.setItem('cart',JSON.stringify(cart));
             showMiniCart();
         });
+        $('.plus').on('click', function(){
+            var id = $(this).attr('data-art');
+            cart[id].quantity++;
+            localStorage.setItem('cart',JSON.stringify(cart));
+            showMiniCart();
+        });
+        $('.cross-img').on('click', function(){
+            var id = $(this).attr('data-art');
+            cart[id].quantity=0;
+            delete cart[id];
+            showMiniCart();
+            localStorage.setItem('cart',JSON.stringify(cart));
+        });
+
     }else{
         var out='<p>Cart is empty</p>';
         $('.cart-content').html(out);
@@ -188,12 +304,4 @@ function checkCart() {
     if (localStorage.getItem('cart') != null) {
         cart = JSON.parse(localStorage.getItem('cart'));
     }
-}
-
-function isEmpty(obj) {
-    for(var i in obj) {
-        if(obj.hasOwnProperty(i))
-            return false;
-    }
-    return JSON.stringify(obj) === JSON.stringify({});
 }
